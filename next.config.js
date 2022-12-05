e45259ffb77e01316180cefa93119360e5e4eeef
Ganesh <ganeshmunisif@gmail.com>
2022-12-05 22:03:32 +0530

    version 1

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withPWA(nextConfig)
